9c258173a5bc19bb8ee949ba448440b039353e31
skcc321 <skcc321@gmail.com>
2016-01-30 18:48:41 +0200

    jj binding

--- config.fish
+++ config.fish
@@ -1,11 +1,17 @@
 fish_vi_mode
-set -gx TERM xterm-256color-italic;
+
+set -gx TERM xterm-256color-italic
+set -Ux EDITOR vim
+set -g fish_key_bindings jj_vi_binding
 
 set fish_greeting ''
 set fish_color_user FF0F3A
 set fish_color_host 8224FF
 set fish_color_pwd FFAF0F
 set fish_color_join WHITE
+set fish_color_error 17C6FF
 rvm default
 
 test $TERM != "screen"; and tmux
+
+
---
+++ functions/jj_vi_binding.fish
@@ -0,0 +1,4 @@
+function jj_vi_binding
+  fish_vi_key_bindings
+  bind -M insert -m default jj force-repaint
+end
